fd98aea90d2c5fc079418f32e315597cd7706de9
Sonam <200517014@student.georgianc.on.ca>
2023-06-30 17:52:25 -0400

    sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#####################################################
+##################################################################################################
 
 #if the script we run has root privilage and if not, exit.
 if  [ "$(id -u)" != 0 ] ; then 
@@ -8,17 +8,12 @@ if  [ "$(id -u)" != 0 ] ; then
 	exit 1
 fi
 
-lshwOutput=$(lshw)
-lscpuOutput=$(lscpu)
-lsblkOutput=$(lsblk)
-dmidecodeOutput=$(dmidecode)
-######################################################
+source ./reportfunctions.sh
+
+###################################################################################################
 
 #Heading and command for system’s fully-qualified domain name
 FQDN=$(hostname -f)
-
-#Heading and command for The operating system name and version
-Host_Information=$(hostnamectl | awk 'FNR == 7 {print $3, $4, $5}')
 
 #Heading and command for the  IP addresses
 IP_Address=$(hostname -I)
@@ -26,370 +21,61 @@ IP_Address=$(hostname -I)
 #Heading and command for amount of space available
 Space_Available=$(df / -h | awk 'FNR == 2 {print $4}')
 
-##############################################################################################
+##############################  error message to the user on stderr #################################
 
-#Cmd to get the computer manufacturer.
-cmpmanuftr=$(echo "$lshwOutput" -class system | grep 'vendor:' | head -1 | awk '{$1=""; print $0}') 
-if [ -z "$cmpmanuftr" ]; then
-	cmpmanuftr='Error! Data is unavailable.'
-else
-	cmpmanuftr=$(echo "$lshwOutput" -class system | grep 'vendor:' | head -1 | awk '{$1=""; print $0}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the Computer description or model.
-cmpdescription=$(echo "$lshwOutput" -class system | grep -m1 -i 'description:' | awk '{print $2}')
-if [ -z "$cmpdescription" ]; then 
-	cmpdescription='Error! Data is unavailable.'
-else
-	cmpdescription=$(echo "$lshwOutput" -class system | grep -m1 -i 'description:' | awk '{print $2}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the Computer serial number.
-cmpserial=$(echo "$lshwOutput" -class system | grep -m1 -i 'serial: ' | awk '{$1=""; print $0}')
-if [ -z "$cmpdescription" ]; then 
-	cmpserial='Error! Data is unavailable.'
-else
-	cmpserial=$(echo "$lshwOutput" -class system | grep -m1 -i 'serial: ' | awk '{$1=""; print $0}')
-fi
+log_errors() {
+    somecommand 2> >(logger -t "$(basename "$0")" -i -p user.warning)
+}
 
-###############################################################################################
+# usage
+log_errors
 
-#Cmd to get the cpu manufacturer.
-cpumanufac=$(echo "$lshwOutput" | grep 'Model name:' | sed 's/,"Model name: *//') 
-if [ -z "$cmpdescription" ]; then 
-	cpumanufac='Error! Data is unavailable.'
-else
-	cpumanufac=$(echo "$lscpuOutput" | grep 'Model name:' | sed 's/,"Model name: *//')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the Cpu architecture.
-cpuarchi=$(echo "$lscpuOutput" | grep 'Architecture: ' | awk '{print$2}')
-if [ -z "$cmpdescription" ]; then 
-	cpuarchi='Error! Data is unavailable.'
-else
-	cpuarchi=$(echo "$lscpuOutput" | grep 'Architecture: ' | awk '{print$2}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the Cpu core count.
-cpucore=$(echo "$lscpuOutput" | grep 'CPU(s): ' | head -1 | awk '{print$2}')
-if [ -z "$cmpdescription" ]; then 
-	cpucore='Error! Data is unavailable.'
-else
-	cpucore=$(echo "$lscpuOutput" | grep 'CPU(s): ' | head -1 | awk '{print$2}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the Cpu maximum speed.
-cpuspeed=$(echo "$lshwOutput" -class cpu | grep 'capacity: ' | head -1 | awk '{print$2}')
-if [ -z "$cmpdescription" ]; then 
-	cpuspeed='Error! Data is unavailable.'
-else
-	cpuspeed=$(echo "$lshwOutput" -class cpu | grep 'capacity: ' | head -1 | awk '{print$2}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the size of L1d.
-sizeL1d=$(echo "$lscpuOutput" | grep 'L1d ' | awk '{print $3}')
-if [ -z "$cmpdescription" ]; then 
-	sizeL1d='Error! Data is unavailable.'
-else
-	sizeL1d=$(echo "$lscpuOutput" | grep 'L1d ' | awk '{print $3}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the size of L1i.
-sizeL1i=$(echo "$lscpuOutput" | grep 'L1i ' | awk '{print $3}')
-if [ -z "$cmpdescription" ]; then 
-	sizeL1i='Error! Data is unavailable.'
-else
-	sizeL1i=$(echo "$lscpuOutput" | grep 'L1i ' | awk '{print $3}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the size of L2.
-sizeL2=$(echo "$lscpuOutput" | grep 'L2' | awk '{print $3}')
-if [ -z "$cmpdescription" ]; then 
-	sizeL2='Error! Data is unavailable.'
-else
-	sizeL2=$(echo "$lscpuOutput" | grep 'L2 ' | awk '{print $3}')
-fi
-#-----------------------------------------------------------------------------------------------
-#Cmd to get the size of L3.
-sizeL3=$(echo "$lscpuOutput" | grep 'L3' | awk '{print $3}')
-if [ -z "$cmpdescription" ]; then 
-	sizeL3='Error! Data is unavailable.'
-else
-	sizeL3=$(echo "$lscpuOutput" | grep 'L3 ' | awk '{print $3}')
-fi
-#####################################################
-
-#Cmd to get Os distro version.
-distroversion=$(hostnamectl | grep 'Kernel: ' | awk '{$1=""; print $0}')
-if [ -z "$cmpdescription" ]; then 
-	distroversion='Error! Data is unavailable.'
-else
-	distroversion=$(hostnamectl | grep 'Kernel: ' | awk '{$1=""; print $0}')
-fi
-
-####################### RAM INFORMATION ##############################
-
-ramManufacturer=$(echo "$dmidecodeOutput" | grep -m1 -i "manufacturer" | awk '{$1=""; print $0}')
-if [ -z "$ramManufacturer" ]; then 
-	ramManufacturer='Error! Data is unavailable.'
-else
-	ramManufacturer=$(echo "$dmidecodeOutput" | grep -m1 -i "manufacturer" | awk '{$1=""; print $0}')
-fi
-
-ramProductName=$(echo "$dmidecodeOutput" | grep -m1 -i "Product name" | awk '{$1=""; $2=""; print $0}')
-if [ -z "$ramProductName" ]; then 
-	ramProductName='Error! Data is unavailable.'
-else
-	ramProductName=$(echo "$dmidecodeOutput" | grep -m1 -i "Product name" | awk '{$1=""; $2=""; print $0}')
-fi
-
-ramSerialNum=$(echo "$dmidecodeOutput" | grep -m1 -i "serial number" | awk '{$1=""; print $0}')
-if [ -z "$ramSerialNum" ]; then 
-	ramSerialNum='Error! Data is unavailable.'
-else
-	ramSerialNum=$(echo "$dmidecodeOutput" | grep -m1 -i "serial number" | awk '{$1=""; print $0}')
-fi
-
-ramSize=$(echo "$lshwOutput" | grep -A10 "\-memory" | grep 'size:' | awk 'FNR == 2 {$1=""; print $0}')
-if [ -z "$ramSize" ]; then 
-	ramSize='Error! Data is unavailable.'
-else
-	ramSize=$(echo "$lshwOutput" | grep -A10 "\-memory" | grep 'size:' | awk 'FNR == 2 {$1=""; print $0}')
-fi
-
-ramSpeed=$(echo "$dmidecodeOutput" --type 17 | grep -m1 Speed | awk '{$1=""; print $0}')
-if [ -z "$ramSpeed" ]; then 
-	ramSpeed='Error! Data is unavailable.'
-else
-	ramSpeed=$(echo "$dmidecodeOutput" --type 17 | grep -m1 Speed | awk '{$1=""; print $0}')
-fi
-
-ramLocation=$(echo "$lshwOutput" | grep -m1 'slot: RAM' |awk '{$1=""; $2=""; print $0}')
-if [ -z "$ramLocation" ]; then 
-	ramLocation='Error! Data is unavailable.'
-else
-	ramLocation=$(echo "$lshwOutput" | grep -m1 'slot: RAM' |awk '{$1=""; $2=""; print $0}')
-fi
-
-totalSize=$(echo "$lshwOutput" | grep -A5 "\-memory" | grep -m1 'size:' | awk '{$1=""; print $0}')
-if [ -z "$totalSize" ]; then 
-	totalSize='Error! Data is unavailable.'
-else
-	totalSize=$(echo "$lshwOutput" | grep -A5 "\-memory" | grep -m1 'size:' | awk '{$1=""; print $0}')
-fi
-
-ramTable=$(paste -d ';' <(echo "$ramManufacturer") <(echo "$ramProductName") <(echo "$ramSerialNum") <(echo "$ramSize") <(echo "$ramSpeed") <(echo "$ramLocation") <(echo "$totalSize") | column -N Manufacturer,Model,'Seial Number',Size,Speed,Location,'Total size' -s ';' -o ' | ' -t)
-
-####################### Storage Information ##############################
-
-driveManufacturer0=$(echo "$lshwOutput" | grep -A10 '\*\-disk' | grep 'vendor:' | awk '{$1=""; print $0}')
-if [ -z "$driveManufacturer0" ]; then 
-	driveManufacturer0='Error! Data is unavailable.'
-else
-	driveManufacturer0=$(echo "$lshwOutput" | grep -A10 '\*\-disk' | grep 'vendor:' | awk '{$1=""; print $0}')
-fi
-
-drivevendor1=$(echo "$lshwOutput" | grep -m1 -A8 "\-volume:0" | grep 'vendor:' | awk '{$1=""; print $0}')
-if [ -z "$drivevendor1" ]; then 
-	drivevendor1='Error! Data is unavailable.'
-else
-	drivevendor1=$(echo "$lshwOutput" | grep -m1 -A8 "\-volume:0" | grep 'vendor:' | awk '{$1=""; print $0}')
-fi
-
-drivevendor2=$(echo "$lshwOutput" | grep -m1 -A8 "\-volume:1" | grep 'vendor:' | awk '{$1=""; print $0}')
-if [ -z "$drivevendor1" ]; then 
-	drivevendor2='Error! Data is unavailable.'
-else
-	drivevendor2=$(echo "$lshwOutput" | grep -m1 -A8 "\-volume:1" | grep 'vendor:' | awk '{$1=""; print $0}')
-fi
-
-drivevendor3=$(echo "$lshwOutput" | grep -m1 -A8 "\-volume:2" | grep 'vendor:' | awk '{$1=""; print $0}')
-if [ -z "$drivevendor3" ]; then 
-	drivevendor3='Error! Data is unavailable.'
-else
-	drivevendor3=$(echo "$lshwOutput" | grep -m1 -A8 "\-volume:2" | grep 'vendor:' | awk '{$1=""; print $0}')
-fi
-
-driveModel=$(echo "$lshwOutput" | grep -m1 -A10 "\-disk" | grep 'product:' | awk '{$1=""; print $0}')
-if [ -z "$driveModel" ]; then 
-	driveModel='Error! Data is unavailable.'
-else
-	driveModel=$(echo "$lshwOutput" | grep -m1 -A10 "\-disk" | grep 'product:' | awk '{$1=""; print $0}')
-fi
-
-driveSize0=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==1 {print $4"B"}')
-if [ -z "$driveSize0" ]; then 
-	driveSize0='Error! Data is unavailable.'
-else
-	driveSize0=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==1 {print $4"B"}')
-fi
-
-driveSize1=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==2 {print $4"B"}')
-if [ -z "$driveSize1" ]; then 
-	driveSize1='Error! Data is unavailable.'
-else
-	driveSize1=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==2 {print $4"B"}')
-fi
-
-driveSize2=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==3 {print $4"B"}')
-if [ -z "$driveSize2" ]; then 
-	driveSize2='Error! Data is unavailable.'
-else
-	driveSize2=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==3 {print $4"B"}')
-fi
-
-driveSize3=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==4 {print $4"B"}')
-if [ -z "$driveSize3" ]; then 
-	driveSize3='Error! Data is unavailable.'
-else
-	driveSize3=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==4 {print $4"B"}')
-fi
-
-drivePartition0=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==1 {print $1}')
-if [ -z "$drivePartition0" ]; then 
-	drivePartition0='Error! Data is unavailable.'
-else
-	drivePartition0=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==1 {print $1}')
-fi
-
-drivePartition1=$(echo "$lsblkOutput" | grep "sda1" | awk 'FNR==1 {print $1}' | tail -c 5)
-if [ -z "$drivePartition1" ]; then 
-	drivePartition1='Error! Data is unavailable.'
-else
-	drivePartition1=$(echo "$lsblkOutput" | grep "sda1" | awk 'FNR==1 {print $1}' | tail -c 5)
-fi
-
-drivePartition2=$(echo "$lsblkOutput" | grep "sda2" | awk 'FNR==1 {print $1}' | tail -c 5)
-if [ -z "$drivePartition2" ]; then 
-	drivePartition2='Error! Data is unavailable.'
-else
-	drivePartition2=$(echo "$lsblkOutput" | grep "sda2" | awk 'FNR==1 {print $1}' | tail -c 5)
-fi
-
-drivePartition3=$(echo "$lsblkOutput" | grep "sda3" | awk 'FNR==1 {print $1}' | tail -c 5)
-if [ -z "$drivePartition3" ]; then 
-	drivePartition3='Error! Data is unavailable.'
-else
-	drivePartition3=$(echo "$lsblkOutput" | grep "sda3" | awk 'FNR==1 {print $1}' | tail -c 5)
-fi
-
-driveMountPoint0=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==1 {print $7}')
-if [ -z "$driveMountPoint0" ]; then 
-	driveMountPoint0='Error! Data is unavailable.'
-else
-	driveMountPoint0=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==1 {print $7}')
-fi
-
-driveMountPoint1=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==2 {print $7}')
-if [ -z "$driveMountPoint1" ]; then 
-	driveMountPoint1='Error! Data is unavailable.'
-else
-	driveMountPoint1=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==2 {print $7}')
-fi
-
-driveMountPoint2=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==3 {print $7}')
-if [ -z "$driveMountPoint2" ]; then 
-	driveMountPoint2='Error! Data is unavailable.'
-else
-	driveMountPoint2=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==3 {print $7}')
-fi
-
-driveMountPoint3=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==4 {print $7}')
-if [ -z "$driveMountPoint3" ]; then 
-	driveMountPoint3='Error! Data is unavailable.'
-else
-	driveMountPoint3=$(echo "$lsblkOutput" | grep "sda" | awk 'FNR==4 {print $7}')
-fi
-
-driveFilesystemSizeSDA2=$(echo "$lsblkOutput" | grep "sda2" | awk '{print $2"B"}')
-if [ -z "$driveFilesystemSizeSDA2" ]; then 
-	driveFilesystemSizeSDA2='Error! Data is unavailable.'
-else
-	driveFilesystemSizeSDA2=$(echo "$lsblkOutput" | grep "sda2" | awk '{print $2"B"}')
-fi
-
-driveFilesystemSizeSDA3=$(echo "$lsblkOutput" | grep "sda3" | awk '{print $2"B"}')
-if [ -z "$driveFilesystemSizeSDA3" ]; then 
-	driveFilesystemSizeSDA3='Error! Data is unavailable.'
-else
-	driveFilesystemSizeSDA3=$(echo "$lsblkOutput" | grep "sda3" | awk '{print $2"B"}')
-fi
-
-driveFilesystemFreeSDA2=$(echo "$lsblkOutput" | grep "sda2" | awk '{print $4"B"}')
-if [ -z "$driveFilesystemFreeSDA2" ]; then 
-	driveFilesystemFreeSDA2='Error! Data is unavailable.'
-else
-	driveFilesystemFreeSDA2=$(echo "$lsblkOutput" | grep "sda2" | awk '{print $4"B"}')
-fi
-
-driveFilesystemFreeSDA3=$(echo "$lsblkOutput" | grep "sda3" | awk '{print $4"B"}')
-if [ -z "$driveFilesystemFreeSDA3" ]; then 
-	driveFilesystemFreeSDA3='Error! Data is unavailable.'
-else
-	driveFilesystemFreeSDA3=$(echo "$lsblkOutput" | grep "sda3" | awk '{print $4"B"}')
-fi
+############################# error message with a timestamp #######################################
 
 
-diskTable=$(paste -d ';' <(echo "$drivePartition0" ; echo "$drivePartition1" ; echo "$drivePartition2" ; echo "$drivePartition3") <(echo "$driveManufacturer0" ; echo "$drivevendor1" ; echo "$drivevendor2" ; echo "$drivevendor3") <(echo "$driveModel" ; echo "N/A" ; echo "N/A" ; echo "N/A") <(echo "$driveSize0" ; echo "$driveSize1" ; echo "$driveSize2" ; echo "$driveSize3") <(echo "N/A" ; echo "N/A" ; echo "$driveFilesystemSizeSDA2" ; echo "$driveFilesystemSizeSDA3") <(echo "N/A" ; echo "N/A" ; echo "$driveFilesystemFreeSDA2" ; echo "$driveFilesystemFreeSDA3") <(echo "$driveMountPoint0" ; echo "$driveMountPoint1" ; echo "$driveMountPoint2" ; echo "$driveMountPoint3") | column -N 'Partition Name (/dev/sda)',Vendor,Model,Size,'Filesystem Size','Filesystem Free Space','Mount Point' -s ';' -o ' | ' -t) 
+verbose=false
+systemReport=false
+diskReport=false
+networkReport=false
+fullReport=false
 
-####################### Video Report ##############################
+function save_error_log() {
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    local error_message="$1"
+    echo "[$timestamp] $error_message" >> /var/log/systeminfo.log
+    if [["$verbose"=true]]; then 
+    echo "Error has occured at [$timestamp] for invalid option: $error_message ; Refer to help section (sysinfo.sh -h)"
+    fi
+    
+}
 
-videoManu=$(lshw -class display | grep "vendor:" | awk '{print $2}')
-if [ -z "$videoManu" ]; then 
-	videoManu='Error! Data is unavailable.'
-else
-	videoManu=$(lshw -class display | grep "vendor:" | awk '{print $2}')
-fi
-
-videoDescription=$(lshw -class display | grep "description:" | awk '{print $2}')
-if [ -z "$videoDescription" ]; then 
-	videoDescription='Error! Data is unavailable.'
-else
-	videoDescription=$(lshw -class display | grep "description:" | awk '{print $2}')
-fi
+#This function displays help information if the user asks for it on the command line or gives us a bad command line
+function displayhelp {
+cat <<EOF 
+  Usage: sysconfig [OPTION]...
+  [OPTIONS]       [DESCRIPTION]
+  --help:         Displays Help Information\n
+  --disk:         Displays Disk Information\n
+  --network:      Runs only the network report\n
+  --system:       runs only the computerreport, osreport, cpureport, ramreport, and videoreport
+  --verbose:      runs your script verbosely, showing any errors to the user instead of sending them to the logfile
+EOF
+}
 
 
 cat <<EOF
 
 ----------------------------Report Content------------------------------
 
-=======================================================================
--SYSTEM DESCRIPTION-
-
-Computer manufacturer=         $cmpmanuftr
-Computer description or model=  $cmpdescription
-Computer serial number=        $cmpserial
-
-========================================================================
--CPU INFORMATION-
-
-CPU manufacturer and model=$cpumanufac
-CPU architecture=              $cpuarchi
-CPU core count=                $cpucore
-CPU maximum speed=             $cpuspeed
-Size of caches (L1d)=          $sizeL1d
-Size of caches (L1i)=          $sizeL1i
-Size of caches (L2)=           $sizeL2
-Size of caches (L3)=           $sizeL3
-
-$ramTable
-========================================================================
--OS INFOPRMATION-
-
-Linux distro=                  $Host_Information
-Distro version=               $distroversion
-
-========================================================================
-
 FQDN= $FQDN
 Operating system name and version= $Host_Information
 IP Address= $IP_Address
 Root filesystem space line and Space Available= $Space_Available
-
-========================================================================
-$diskTable
 EOF
-
-
-
-
+cpuReport
+networkReport
+storageReport
+ramReport
+videoReport
+cmpReport
+osReport
